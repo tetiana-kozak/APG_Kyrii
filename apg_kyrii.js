192d03ff1dcb509bd0d03e28763ed62bf87db2fe
tetiana-kozak <kozaktanya077@gmail.com>
2022-04-21 16:40:13 +0300

    Counter add +1

--- apg_kyrii.js
+++ apg_kyrii.js
@@ -1,4 +1,16 @@
+let counter = 0
+let element = document.getElementById ("divCounter")
+
+
 console.log ("Hello")
+
 function sayClick (){
     alert ("You on Anna Kyrii site")
+}
+
+
+function countClick (){
+    counter ++
+    element.innerText = counter
+    console.log (counter)
 }
