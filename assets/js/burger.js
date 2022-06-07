c75760664c95c82b7d12b131e6dd30622f5c65b9
APG\t.kozak <kozaktanya077@gmail.com>
2022-06-07 11:47:18 +0300

    Add smooth scroll on a page using JS

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,5 +1,7 @@
 "use strict"
 
+            // Burger menu
+
 const menuIcon = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 
@@ -12,14 +14,36 @@ if (menuIcon) {
     
 };
 
-if (menuIcon.classList.contains('_active')) {
-    document.body.classList.remove('_lock');
-    menuIcon.classList.remove('_active');
-    menuBody.classList.remove('_active');    
+
+
+
+            // Scroll
+
+const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+if (menuLinks.length > 0) {
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener("click", onMenuLinksClick);
+    });
+
+    function onMenuLinksClick(e){
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY - document.querySelector('.page__header').offsetHeight;
+
+            if (menuIcon.classList.contains('_active')) {
+                document.body.classList.remove('_lock');
+                menuIcon.classList.remove('_active');
+                menuBody.classList.remove('_active');    
+            }
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: "smooth"
+            });
+
+            e.preventDefault();
+        }
+    }
 }
-
-
-
-
-
 
