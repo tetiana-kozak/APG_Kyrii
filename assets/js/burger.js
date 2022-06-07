9d7c101d3845acf1313b7f7c12b9b889f7d75e81
APG\t.kozak <kozaktanya077@gmail.com>
2022-06-07 10:46:46 +0300

    Made burger menu working (open and close)

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,5 +1,25 @@
 "use strict"
 
+const menuIcon = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if (menuIcon) {
+    menuIcon.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock');
+        menuIcon.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+    
+};
+
+if (menuIcon.classList.contains('_active')) {
+    document.body.classList.remove('_lock');
+    menuIcon.classList.remove('_active');
+    menuBody.classList.remove('_active');    
+}
+
+
+
 
 
 
