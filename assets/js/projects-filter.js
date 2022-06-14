58b55fe7300a5b96e41e2412596b31668e1b69b3
APG\t.kozak <kozaktanya077@gmail.com>
2022-06-14 17:08:19 +0300

    Add animation in projects filter

---
+++ assets/js/projects-filter.js
@@ -0,0 +1,40 @@
+"use strict"
+
+const filterList = document.querySelector('.filter__navigation')
+// const filterList = document.querySelector('.filter__navigation-list')
+const projects = document.querySelectorAll('.project__content')
+
+    
+filterList.addEventListener('click', function(evt){
+    // console.log('click')
+    if(evt.target.tagName !== 'LI') return false;
+
+    let filterClass = evt.target.dataset['filter']
+    console.log(filterClass)
+
+    
+    projects.forEach(element => {
+        
+        element.classList.remove('anime')        
+        element.classList.remove('filter_hidden')        
+        if (!element.classList.contains(filterClass) && filterClass !== 'all') {
+            // console.log("inside if")
+            element.classList.add('anime')        
+            setTimeout(element.classList.add('filter_hidden'), 1500)       
+            // projects.forEach(card => {
+            //     card.ontransitionend = function () {
+            //         if (card.classList.contains('anime')) {
+            //             card.classList.add('filter_hidden')
+            //         }
+            //     }
+            // });
+        }
+
+    
+    });
+
+
+})
+
+
+
