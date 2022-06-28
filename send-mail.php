651f895bb02a876b82fd4961324c8aed8a784546
APG\t.kozak <kozaktanya077@gmail.com>
2022-06-28 17:04:52 +0300

    Fixed contact us form send

--- send-mail.php
+++ send-mail.php
@@ -1,40 +1,40 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\\Exception;
+    use PHPMailer\PHPMailer\Exception;
 
-    require 'phpmailer/src/Exception';
-    require 'phpmailer/src/PHPMailer';
+    require 'phpmailer/src/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
 
     $mail = new PHPMailer(true);
     $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language');
+    $mail->setLanguage('ru', 'phpmailer/language/');
     $mail->IsHTML(true);
 
     // Від кого лист
-    $mail->SetFrom('info@ls.guru', 'Letter from');
+    $mail->SetFrom('forspam035@ukr.net', 'Letter from');
     // Кому лист
     $mail->addAddress('kozaktanya077@gmail.com');
     // Тема листа
-    $mail->Subject = 'Letter theme';
+    $mail->Subject = 'Online form';
 
     // Тіло листа
 
     $body = '<h1>You recieve a letter from online form in website!</h1>';
 
     if (trim(!empty($_POST['name']))) {
-        $body.='<p><strong>Ім`я:</strong> '.$_POST['name'].'</strong>';
+        $body.='<p><strong>Name:</strong> '.$_POST['name'].'</strong>';
     }
 
     if (trim(!empty($_POST['phone']))) {
-        $body.='<p><strong>Телефон:</strong> '.$_POST['phone'].'</strong>';
+        $body.='<p><strong>Phone:</strong> '.$_POST['phone'].'</strong>';
     }
 
     if (trim(!empty($_POST['email']))) {
-        $body.='<p><strong>Емейл:</strong> '.$_POST['email'].'</strong>';
+        $body.='<p><strong>Email:</strong> '.$_POST['email'].'</strong>';
     }
 
     if (trim(!empty($_POST['message']))) {
-        $body.='<p><strong>Повідомлення:</strong> '.$_POST['message'].'</strong>';
+        $body.='<p><strong>Message:</strong> '.$_POST['message'].'</strong>';
     }
 
     $mail->Body = $body;
