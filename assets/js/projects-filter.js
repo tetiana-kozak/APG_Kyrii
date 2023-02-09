c5f37ebe16ae165591a1edb1100e55f528eae70b
Tetiana <kozaktanya077@gmail.com>
2023-02-09 23:07:09 +0200

    refactored code in project-filter

--- assets/js/projects-filter.js
+++ assets/js/projects-filter.js
@@ -1,99 +1,60 @@
 "use strict"
 
-// function app() {
-    const filterList = document.querySelectorAll('.filter__btn')
-    const allProjects = document.querySelectorAll('.project__content')
-    const changeCategoryName = document.querySelector('.arrow-text')
-    // get params = public (qvery param)
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    console.log('urlSearchParams :>> ', urlSearchParams);
-    const params = Object.fromEntries(urlSearchParams.entries());
-    // console.log('params :>> ', params);
-    console.log('params?.filter :>> ', params?.filter);
-    if (params?.filter) {
-        filter(params.filter, allProjects)
-        
-    }
+const filterList = document.querySelectorAll('.filter__btn')
+const allProjects = document.querySelectorAll('.project__content')
+const changeCategoryName = document.querySelector('.arrow-text')
 
-    function firstLetterToUppercase(category){
-        return category.charAt(0).toUpperCase() + category.slice(1);
-    }
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
 
-    function filter (category, items) {
-        console.log('inside');
+if (params?.filter) {
+    filter(params.filter, allProjects)
+}
 
-        items.forEach(item => {
-            const isItemFiltered = !item.classList.contains(category)
-            const isShowAll = category.toLowerCase() === 'all projects'
-            if (isItemFiltered && !isShowAll) {
-                item.classList.add('animation')
-                // setUrlParams(currentCategory)
-                
-            } else{
-                item.classList.remove('filter_hidden')
-                item.classList.remove('animation')
+filterList.forEach((button) => {
+
+    const currentCategory = button.dataset.filter
+
+    button.addEventListener('click', () => {
+        changeUrl(currentCategory)
+    })
+
+    allProjects.forEach(card => {
+        card.ontransitionend = function () {
+            if (card.classList.contains('animation')) {
+                card.classList.add('filter_hidden')
             }
-            changeCategoryName.innerHTML = firstLetterToUppercase(category);
-            // deleteUrlParams()
-            // changeUrl()
-
-
-        });
-
-    }
-
-    function changeUrl(currentCategory){
-        const currentUrl = window.location.href
-        let currentParam = window.location.search
-        console.log('here');
-
-        console.log('window.location.search before :>> ', window.location.search);
-
-        
-        const filterParams = {
-            filter: currentCategory
         }
-
-        const searchParams = new URLSearchParams(filterParams)
-        const queryString = searchParams.toString()
-        // console.log('searchParams :>> ', searchParams);
-        // console.log('queryString :>> ', queryString);
-         
-        //  button.addEventListener('click', () => {
-
-            window.location.search = queryString
-            console.log('window.location.search click :>> ', window.location.search);
-        // })
-    }
-
-    filterList.forEach((button) => {
-        const currentCategory = button.dataset.filter
-
-
-        button.addEventListener('click', () => {
-            // const currentCategory = button.dataset.filter
-            // console.log('currentCategory :>> ', currentCategory);
-
-            // filter(currentCategory, allProjects)
-            // setUrlParams(currentCategory)
-            changeUrl(currentCategory)
-
-
-            
-        })
-
-        allProjects.forEach(card => {
-            card.ontransitionend = function () {
-                if (card.classList.contains('animation')) {
-                    card.classList.add('filter_hidden')
-                }
-            }
-        });
-
-
-
-
-        
     });
-// }
-// app()
+});
+
+function filter (category, items) {
+    items.forEach(item => {
+        const isItemFiltered = !item.classList.contains(category)
+        const isShowAll = category.toLowerCase() === 'all projects'
+        if (isItemFiltered && !isShowAll) {
+            item.classList.add('animation')
+        } else{
+            item.classList.remove('filter_hidden')
+            item.classList.remove('animation')
+        }
+        changeCategoryName.innerHTML = firstLetterToUppercase(category);
+    });
+}
+
+function firstLetterToUppercase(category){
+    return category.charAt(0).toUpperCase() + category.slice(1);
+}
+
+function changeUrl(currentCategory){
+
+    const filterParams = {
+        filter: currentCategory
+    }
+    const searchParams = new URLSearchParams(filterParams)
+    const queryString = searchParams.toString()
+
+    window.location.search = queryString
+}
+
+
