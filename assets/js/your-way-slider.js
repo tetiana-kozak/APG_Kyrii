c6241297d0287920ee8fad7b82a9e2bdf6abd3d7
Tetiana <kozaktanya077@gmail.com>
2026-04-12 19:30:34 +0300

    add slider your way eng

---
+++ assets/js/your-way-slider.js
@@ -0,0 +1,15 @@
+new Swiper('.your-way__swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    centeredSlides: true,
+    mousewheel: {
+        enabled: true,
+        eventsTarget: '.your-way__swiper',
+        sensitivity: 1,
+    },
+    navigation: {
+        nextEl: '.your-way__arrow',
+    },
+    speed: 800,
+    keyboard: { enabled: true },
+});
